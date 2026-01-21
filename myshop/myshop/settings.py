ea62ce73c012eb82cd2bdbd83bb39bc33b7090a1
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-01-21 14:58:57 +0000

    Fix: adds missing comma in INSTALLED_APPS

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig', 
 ]
 
