236098f01c3f82bd26006ccb7d9f912036089041
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-02-04 17:14:38 +0000

    fix: correct order.id typo to order_id for setting the order_id

--- myshop/payment/views.py
+++ myshop/payment/views.py
@@ -11,7 +11,7 @@ stripe.api_version = settings.STRIPE_API_VERSION
 
 
 def payment_process(request):
-    order_id = request.session.get('order.id')
+    order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
 
     if request.method == 'POST':
