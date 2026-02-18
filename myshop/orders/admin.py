8b9038a00ffd72d49a8bdd5ad6986bce9788d561
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-02-18 14:48:17 +0000

    fix: comments newly added code to trouble shoot payment confirmation not showing in admin

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 from .models import Order, OrderItem
-from django.utils.safestring import mark_safe
+#from django.utils.safestring import mark_safe
 
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
 
-def order_payment(obj):
+"""def order_payment(obj):
     url = obj.get_stripe_url()
     if obj.stripe_id:
         html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
         return mark_safe(html)
     return ''
 order_payment.short_description = 'Stripe payment'
-
+"""
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -26,7 +26,7 @@ class OrderAdmin(admin.ModelAdmin):
         'postal_code',
         'city',
         'paid',
-        order_payment,
+        #order_payment,
         'created',
         'updated'
     ]
