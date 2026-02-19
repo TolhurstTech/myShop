761bb5406d8f512e23699b76f6830cff9a3840cc
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-02-19 15:01:46 +0000

    feat: create order detail view for the admin panel

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import redirect, render
-
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, redirect, render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
@@ -34,4 +34,12 @@ def order_create(request):
         request,
         'orders/order/create.html',
         {'cart': cart, 'form': form}
+    )
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(
+        request, 'admin/orders/order/detail.html', {'order': order}
     )
