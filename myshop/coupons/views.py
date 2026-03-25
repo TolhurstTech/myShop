31eb11d384466f4643b23c603e205a8e2b216f2c
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-03-25 18:32:49 +0000

    feat: add logic to check coupon status

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -11,14 +11,31 @@ def coupon_apply(request):
     form = CouponApplyForm(request.POST)
     if form.is_valid():
         code = form.cleaned_data['code']
-        try:
-            coupon = Coupon.objects.get(
-                code__iexact=code,
-                valid_from__lte=now,
-                valid_to__gte=now,
-                active=True
-            )
+        coupon_status = is_coupon_valid(code)
+        if coupon_status["is_valid"]:
+            coupon = coupon_status["coupon"]
             request.session['coupon_id'] = coupon.id
-        except Coupon.DoesNotExist:
+        else:
             request.session['coupon_id'] = None
     return redirect('cart:cart_detail')
+
+# my own addition to make coupon validation easier and more detailed across the project
+def is_coupon_valid(coupon_id):
+    now = timezone.now()
+    
+    # Check if coupon exists
+    try:
+        coupon = Coupon.objects.get(code__iexact=coupon_id)
+    except Coupon.DoesNotExist:
+        # add warning message
+        return {"is_valid": False, "reason": "Coupon not found"}
+    
+    if not (coupon.valid_from <= now <= coupon.valid_to):
+        # add warning message
+        return {"is_valid": False, "reason": f"Coupon expired. Valid until {coupon.valid_to}"}
+    
+    if not coupon.active:
+        # add warning message
+        return {"is_valid": False, "reason": "Coupon is inactive"}
+    
+    return {"is_valid": True, "coupon": coupon}
