5b1ad629b341f236d06db437b3c5061ab965c913
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-09-07 16:34:48 +0100

    fix: fixed typo in cart_detail context

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -66,7 +66,7 @@ def cart_detail(request):
         {
             'cart': cart_items,
             'cart_obj': cart,
-            'coupon_apply_form': coupon_apply_form
+            'coupon_apply_form': coupon_apply_form,
             'recommended_products': recommended_products
         }
     )
