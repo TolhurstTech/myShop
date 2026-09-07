da9c5ff9eb6bbdf42f272fa0126ba022fe81b5d2
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-09-07 16:29:18 +0100

    style: removed commented code and cleaned up spacing between logic blocks

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -39,11 +39,11 @@ class Cart:
         product_ids = self.cart.keys()
         # get the product objects and add them to the cart
         products = Product.objects.filter(id__in=product_ids)
-        # shallow copy causes issues passing decimals for coupons and messages to work try deepcopy solution 
-        # cart = self.cart.copy()
         cart = deepcopy(self.cart)
+
         for product in products:
             cart[str(product.id)]['product'] = product
+            
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
