04d2360d3d1b8cd815e080504a4da1a15a2a0d67
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-09-21 15:39:28 +0100

    fix: corrected i18n typo

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.il8n import il8n_patterns
+from django.conf.urls.il8n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = il8n_patterns(
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
