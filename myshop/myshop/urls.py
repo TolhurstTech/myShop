8dc8e890287454d8ed9a46f8796a0a7e4363a604
TolhurstTech <k_tolhurst@hotmail.co.uk>
2026-09-21 16:02:39 +0100

    feat: moved webhook url to main urls to avoid duplicate translation urls breaking payment

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.utils.translation import gettext_lazy as _
+from payment import webhooks
 
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
@@ -30,6 +31,10 @@ urlpatterns = i18n_patterns(
     path('rosetta/', include('rosetta.urls')),
     path('', include('shop.urls', namespace='shop')),
 )
+
+urlpatterns += [
+    path('payment/webhook/', webhooks.stripe_webhook, name='stripe-webhook'),
+]
 
 if settings.DEBUG:
     urlpatterns += static(
